485419ecc349e2efb223345fa7cf1fea21d5cbb4
Pedro Gottlob <95660382+Rude0102@users.noreply.github.com>
2023-05-28 19:25:30 -0300

    Testes

---
+++ __tests__/home/index.test.js
@@ -0,0 +1,24 @@
+import saveData from 'Users/Pedro/OneDrive/Documentos/Trabalhos da faculdade/Mobile Systems/BuscaCep/buscacep/src/pages/home/';
+
+describe("saveData", ()=>{
+
+    describe("saveData", ()=> {
+
+        It("deve salvar as informações do CEP ", ()=> {
+            const resultado = saveData({
+                "cep": "01001-000",
+                "logradouro": "Praça da Sé",
+                "complemento": "lado ímpar",
+                "bairro": "Sé",
+                "localidade": "São Paulo",
+                "uf": "SP",
+                "ibge": "3550308",
+                "gia": "1004",
+                "ddd": "11",
+                "siafi": "7107"
+              });
+            console.log(resultado)
+        });
+
+    })
+});
